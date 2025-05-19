5044910eefbb8924629181e3422b0e8ff443098b
Hua Li <realname@amazon.com>
2025-05-19 23:16:28 +0000

    learning rate can be pushed even higher for AdamW_Atan2

--- run.sh
+++ run.sh
@@ -5,11 +5,15 @@
 # Adam atan2 (decoupled_wd did not work)
 #python -m src.train -project in-context-learning -name test -family san -model_name gpt2 -task conjunction -data boolean -train_steps 15000 -n_dims 28 -n_embd 256 -n_layer 12 -n_head 8 -batch_size 64 -learning_rate 0.0001 -adam_atan2 -gpu 2
 
-# push lr
+# push lr higher
 # AdamW
 #python -m src.train -project in-context-learning -name test -family san -model_name gpt2 -task conjunction -data boolean -train_steps 15000 -n_dims 28 -n_embd 256 -n_layer 12 -n_head 8 -batch_size 64 -learning_rate 0.01 -gpu 1
 
 # Adam atan2 
 #python -m src.train -project in-context-learning -name test -family san -model_name gpt2 -task conjunction -data boolean -train_steps 15000 -n_dims 28 -n_embd 256 -n_layer 12 -n_head 8 -batch_size 64 -learning_rate 0.01 -adam_atan2 -gpu 2
+
+# push lr even higher
+# Adam atan2 
+#python -m src.train -project in-context-learning -name test -family san -model_name gpt2 -task conjunction -data boolean -train_steps 15000 -n_dims 28 -n_embd 256 -n_layer 12 -n_head 8 -batch_size 64 -learning_rate 0.1 -adam_atan2 -gpu 2
 
 
